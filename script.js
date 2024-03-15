512a56e9d661f75223063979c3da732719b4e8ba
Marc-Andrei <marc_andrei_sorin@yahoo.com>
2024-03-15 09:47:45 +0200

    add custom button

--- script.js
+++ script.js
@@ -3,13 +3,38 @@ function randomColor() {
     
 }
 
-const container = document.querySelector(".container");
-for (let j = 0; j <16; j++){
-    for (let i = 0; i < 16; i++) {
-        const grid = document.createElement("div");
-        grid.className = "grid";
-        grid.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()},${randomColor()})`;
-        grid.style.width = 100/16 + "%";
-        container.appendChild(grid);
+function createGrid (squares) {
+    for (let j = 0; j < squares; j++){
+        for (let i = 0; i < squares; i++) {
+            const grid = document.createElement("div");
+            grid.className = "grid";
+            grid.style.boxSizing = "border-box";
+            grid.style.border = "1px solid black";
+            grid.style.backgroundColor = `white`;
+            grid.style.width = 100/squares + "%";
+            container.appendChild(grid);
+        }
     }
 }
+
+const btn = document.querySelector(".btn");
+const container = document.querySelector(".container");
+let pr=16;
+createGrid(pr);
+btn.addEventListener("click", () => {
+    pr = prompt("Select number of squares(not more than 100)");
+    if (pr > 100) {
+        alert("Number too high!");
+    } else{
+        while (container.childElementCount > 0) {
+            container.removeChild(container.firstElementChild);
+        } 
+        createGrid(pr);
+    }
+})
+
+
+
+container.addEventListener("mouseover", e => {
+    e.target.style.backgroundColor = "black";
+});
